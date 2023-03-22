1040505181f4c7d2abb03cfce0ca1331c494aeda
Eliezir <eliezir2004@gmail.com>
2023-03-21 21:39:52 -0300

    login on user

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import 'react-native-gesture-handler';
 import { useState } from 'react';
+
 import WelcomeScreen from './src/screens/WelcomeScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
+import Profile  from './src/screens/Profile'
+
 import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import { initializeApp } from "firebase/app";
@@ -10,7 +13,12 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-//firebase config ...
+  apiKey: "AIzaSyAwSDlGHKEKX_GLIwjp-y6d3AVSkUQRWgs",
+  authDomain: "beblessed-640a7.firebaseapp.com",
+  projectId: "beblessed-640a7",
+  storageBucket: "beblessed-640a7.appspot.com",
+  messagingSenderId: "659842303363",
+  appId: "1:659842303363:web:88664d968d026f184be2b6"
 };
 
 const Stack = createStackNavigator();
@@ -19,6 +27,24 @@ const app = initializeApp(firebaseConfig);
 export default function App() {
   const auth = getAuth();
   const [user, setUser] = useState(auth.currentUser);
+
+  onAuthStateChanged(getAuth(app), (user) => {
+    if (user) {
+      console.log("User logged in...");
+      setUser(user);
+    } else {
+      console.log("User not logged in...");
+      setUser(null);
+    }
+  });
+
+
+  if(user){
+    return(
+      <Profile/>
+    )
+  }
+
   return (
   <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown: false}}>
