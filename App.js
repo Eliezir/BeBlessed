4ceafda571932e0a942bb97e88e3cee449d18776
Eliezir <eliezir2004@gmail.com>
2023-03-23 00:12:03 -0300

    verificando se o usuário está logado

--- App.js
+++ App.js
@@ -5,20 +5,17 @@ import WelcomeScreen from './src/screens/WelcomeScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
 import Profile  from './src/screens/Profile'
+import UserNotVerified from "./src/screens/UserNotVerified"
 
 import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import { initializeApp } from "firebase/app";
 import { onAuthStateChanged } from 'firebase/auth';
 import { getAuth } from 'firebase/auth';
+import * as SecureStore from 'expo-secure-store';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwSDlGHKEKX_GLIwjp-y6d3AVSkUQRWgs",
-  authDomain: "beblessed-640a7.firebaseapp.com",
-  projectId: "beblessed-640a7",
-  storageBucket: "beblessed-640a7.appspot.com",
-  messagingSenderId: "659842303363",
-  appId: "1:659842303363:web:88664d968d026f184be2b6"
+
 };
 
 const Stack = createStackNavigator();
@@ -28,21 +25,43 @@ export default function App() {
   const auth = getAuth();
   const [user, setUser] = useState(auth.currentUser);
 
+   useState(()=>{
+    SecureStore.getItemAsync("user").then((user)=>{
+      setUser(user)
+    })
+   },[])
+ 
   onAuthStateChanged(getAuth(app), (user) => {
     if (user) {
-      console.log("User logged in...");
       setUser(user);
     } else {
-      console.log("User not logged in...");
       setUser(null);
     }
   });
 
+  const validateEmail = async () =>{
+     const userValidate = await auth.currentUser.reload();
+     console.log(userValidate)
+    if (userValidate) {
+      setUser(userValidate);
+    } else {
+      setUser(null);
+    }
+    };
+  
+
 
   if(user){
-    return(
-      <Profile/>
-    )
+    if(user.emailVerified){
+      return(
+        <Profile user={user}/>
+      )
+    }
+    else if(!user.emailVerified){
+      return(
+        <UserNotVerified user={user} checkUser={()=>{validateEmail()}}/>
+      )
+    }
   }
 
   return (
