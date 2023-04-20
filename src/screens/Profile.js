527bc78ebbe6890a0481ff447865d42a3b48dec0
Eliezir Moreira <eliezir2004@gmail.com>
2023-04-20 08:45:57 -0300

    pegando o user do firebase firestore

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   Text,
@@ -9,29 +9,41 @@ import {
 } from "react-native";
 
 import { getAuth } from "firebase/auth";
+
 import Icon from "react-native-vector-icons/Ionicons";
-import { useNavigation } from '@react-navigation/native';
-const {width,height} = Dimensions.get("window")
+import { useNavigation } from "@react-navigation/native";
+const { width, height } = Dimensions.get("window");
+import { signOut } from "../services/AuthServices";
 
-
-import {signOut} from "../services/AuthServices";
+import { getUser } from "../services/userServices";
 
 export default function Profile() {
-const navigation = useNavigation();
-const firebaseAuth = getAuth();
-const user = firebaseAuth.currentUser;
-const [userPhoto, setUserPhoto] = useState(user.photoURL)
+  const navigation = useNavigation();
+  const firebaseAuth = getAuth();
+  const user = firebaseAuth.currentUser;
+  const [userPhoto, setUserPhoto] = useState(user.photoURL);
 
- return (
-   <SafeAreaView style={styles.container}>
-    <View style={styles.header}>
-    <Icon name="chevron-back-outline" size={30} color="#ffff" onPress={()=>signOut()}/>
-    <Icon name="ellipsis-vertical" size={30} color="#ffff" onPress={()=>navigation.navigate("ProfileEdit")}/>
-    </View>
-    
-    <ImageBackground source={{uri:userPhoto}} style={styles.userImage}>
-      <Text style={styles.userName}>{user.displayName}</Text>
-      
+  getUser(user);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Icon
+          name="chevron-back-outline"
+          size={30}
+          color="#ffff"
+          onPress={() => signOut()}
+        />
+        <Icon
+          name="ellipsis-vertical"
+          size={30}
+          color="#ffff"
+          onPress={() => navigation.navigate("ProfileEdit")}
+        />
+      </View>
+
+      <ImageBackground source={{ uri: userPhoto }} style={styles.userImage}>
+        <Text style={styles.userName}>{user.displayName}</Text>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -41,28 +53,27 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#181a20",
     alignItems: "center",
-    flex:1,
+    flex: 1,
   },
   header: {
     top: 15,
     width: "95%",
-    justifyContent:"space-between",
-    flexDirection:"row"
+    justifyContent: "space-between",
+    flexDirection: "row",
   },
-  userImage:{
-    position:'absolute',
-    height:height/2.5,
-    width:width,
-    zIndex:-1,
-    top:0,
-
+  userImage: {
+    position: "absolute",
+    height: height / 2.5,
+    width: width,
+    zIndex: -1,
+    top: 0,
   },
-  userName:{
-    position:'absolute',
-    bottom:-10, 
-    left:35,
-    color:"#fff",
-    fontWeight:"900",
-    fontSize:35
+  userName: {
+    position: "absolute",
+    bottom: -10,
+    left: 35,
+    color: "#fff",
+    fontWeight: "900",
+    fontSize: 35,
   },
 });
