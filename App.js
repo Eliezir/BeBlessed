37094d138dc1feeb455d295779e2da46e487b3b5
Eliezir <eliezir2004@gmail.com>
2023-05-17 02:29:49 -0300

    Limpando o código

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import 'react-native-gesture-handler';
-import {useState } from 'react';
-import UserNotVerified from "./src/screens/UserNotVerified"
+import {useEffect, useState } from 'react';
+import UserNotVerified from "./src/screens/Login/UserNotVerified"
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from "./src/firebase/config";
 import AppNavigation from './src/navigation/AppNavigation';
 import LoginNavigation from './src/navigation/LoginNavigation';
-
+import * as SecureStore from 'expo-secure-store';
 
 
 export default function App() {
  
   const [user, setUser] = useState(auth.currentUser);
- /*  console.log(auth) */
-
  
+ /*  useEffect(() => {
+    SecureStore.getItemAsync("user").then((user) => {
+      if (user) {
+        setUser(JSON.parse(user));
+      }
+    });
+  }, []); */
+
+
   onAuthStateChanged(auth, (user) => {
+ 
     if (user) {
       setUser(user);
     } 
     else {
       setUser(null);
     }
+ 
   });
 
   const updateUser = async () =>{
@@ -37,7 +46,7 @@ export default function App() {
   if(user){
     if(user.emailVerified){
       return(
-      <AppNavigation reloadUser={updateUser}/>
+      <AppNavigation user={user} reloadUser={updateUser}/>
       )
     }
     else if(! user.emailVerified){
